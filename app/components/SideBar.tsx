077dfd818b2b06bd1945ddbc9af6dab3783d024e
YanjunGuo1007 <yanjun.guo@uzh.ch>
2025-05-22 22:02:58 +0200

    Update Home button onHover Text colour

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -60,7 +60,7 @@ const SideBar: React.FC = () => {
         <div className="h-screen w-20 flex flex-col items-center pt-5 z-10">
             <Link href="/logged-in">
                 <button
-                    className="btn btn-neutral hover:bg-base-300 transition-all w-20 mt-2 ml-8 mb-6"
+                    className="btn btn-neutral hover:bg-base-300 hover:text-black transition-all w-20 mt-2 ml-8 mb-6"
                 >
                     Home
                 </button>
